bd9ac7b30d7845f7b3eb7f6ad05535dba96696ca
Unsocialmanx <andrewmcnutt15@gmail.comm>
2020-04-28 12:54:57 -0700

    Delete Food.js

--- Food.js
+++
@@ -1,8 +0,0 @@
-let food = document.getElementByID("food").value;
-let calories = document.getElementByID("calories").value;
-let quantity = document.getElementByID("quantity").value;
-if (food === '') || calories === '' ||quantity === ''){
-document.write("Please fill out all fields")
-}else{
-document.write(food + ", " + calories + ", " + quantity)
-}
